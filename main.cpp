ad76e9ddfc5b1f4639d991ace517a90667bc0150
Hariprasath Krishnamoorthi <hariprasath.krishnamoorthi@psiog.com>
2025-08-17 15:28:22 -0700

    second  commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+char getChoice(string question) {
+    char choice;
+    cout << question << " (y/n): ";
+    cin >> choice;
+    return choice;
+}
+
+void JobDescription() {
+    cout << "Job description defined.\n";
+    cout << "Crew request sent to HR.\n";
+    cout << "Classification determined.\n";
+}
 
 
 int main() {
